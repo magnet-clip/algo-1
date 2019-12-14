178e6ea8d16abee6f901473c7a9a831f8565d422
Rustam Guseynov <guseynov.r.v@gmail.com>
2019-12-14 18:20:31 +0300

    Seems to be done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-#include <algorithm>
-#include <climits>
 #include <exception>
 #include <iostream>
 #include <map>
@@ -73,112 +71,29 @@ class TestRunner {
   int fail_count = 0;
 };
 
-struct Segment {
-  int start, end;
-};
-
-bool operator<(const Segment& a, const Segment& b) { return a.start < b.start; }
-
-vector<int> optimal_points(vector<Segment>& segments) {
-  vector<int> points;
-
-  sort(segments.begin(), segments.end());
-  size_t i = 0;
-  while (i < segments.size()) {
-    auto containing_segment = segments[i];
-    while (++i < segments.size()) {
-      if (segments[i].start > containing_segment.end)
-        break;  // this segment sticks completely out from containing one
-
-      if (segments[i].start > containing_segment.start)
-        containing_segment.start = segments[i].start;
-      if (segments[i].end < containing_segment.end)
-        containing_segment.end = segments[i].end;
+vector<int> optimal_summands(int n) {
+  vector<int> summands;
+  int sum = 0;
+  int i = 1;
+  while (sum < n) {
+    if (sum + i <= n) {
+      summands.push_back(i);
+      sum += i++;
+      if (sum == n) break;
+    } else {
+      summands[summands.size() - 1] += n - sum;
+      break;
     }
-    points.push_back(containing_segment.end);
   }
-  return points;
-}
-
-void TestNoSegments() {
-  vector<Segment> empty;
-  AssertEqual(optimal_points(empty).size(), 0, "0");
-}
-
-void TestOneSegment() {
-  vector<Segment> one{{1, 2}};
-  AssertEqual(optimal_points(one).size(), 1, "{1,2}");
-
-  vector<Segment> two{{1, 1}};
-  AssertEqual(optimal_points(two).size(), 1, "{1,1}");
-}
-
-void TestTwoSegments() {
-  vector<Segment> a{{1, 2}, {1, 2}};
-  AssertEqual(optimal_points(a).size(), 1, "Two equivalent segments");
-
-  vector<Segment> b{{1, 2}, {3, 4}};
-  AssertEqual(optimal_points(b).size(), 2, "Two non-overlapping segments");
-
-  vector<Segment> c{{1, 3}, {2, 4}};
-  AssertEqual(optimal_points(c).size(), 1, "Two overlapping segments");
-
-  vector<Segment> d{{1, 3}, {3, 4}};
-  AssertEqual(optimal_points(d).size(), 1,
-              "Two segments overlapping in single point");
-
-  vector<Segment> e{{2, 4}, {1, 3}};
-  AssertEqual(optimal_points(e).size(), 1, "Two overlapping unsorted segments");
-}
-
-void TestThreeSegments() {
-  vector<Segment> x;
-  x = {{1, 6}, {1, 6}, {1, 6}};
-  AssertEqual(optimal_points(x).size(), 1, "Three equal");
-
-  x = {{1, 6}, {2, 5}, {3, 4}};
-  AssertEqual(optimal_points(x).size(), 1, "Three one in another");
-
-  x = {{2, 5}, {1, 6}, {3, 4}};
-  AssertEqual(optimal_points(x).size(), 1, "Three one in another unsorted");
-
-  x = {{2, 5}, {3, 7}, {4, 5}};
-  AssertEqual(optimal_points(x).size(), 1,
-              "Three segments, second sticks out and third in second");
-
-  x = {{2, 4}, {3, 7}, {5, 6}};
-  AssertEqual(
-      optimal_points(x).size(), 2,
-      "Three segments, second sticks out and third in second but out of first");
-
-  x = {{1, 2}, {2, 3}, {3, 4}};
-  AssertEqual(optimal_points(x).size(), 2,
-              "Three segments, one by one, touching each other");
-
-  x = {{1, 2}, {3, 4}, {5, 6}};
-  AssertEqual(optimal_points(x).size(), 3, "Three separate segments");
-}
-
-void RunAllTests() {
-  TestRunner runner;
-  runner.RunTest(TestNoSegments, "No segments");
-  runner.RunTest(TestOneSegment, "One segment");
-  runner.RunTest(TestTwoSegments, "Two segments");
-  runner.RunTest(TestThreeSegments, "Three segments");
+  return summands;
 }
 
 int main() {
-  RunAllTests();
-
   int n;
-  std::cin >> n;
-  vector<Segment> segments(n);
-  for (size_t i = 0; i < segments.size(); ++i) {
-    std::cin >> segments[i].start >> segments[i].end;
-  }
-  vector<int> points = optimal_points(segments);
-  std::cout << points.size() << "\n";
-  for (size_t i = 0; i < points.size(); ++i) {
-    std::cout << points[i] << " ";
+  cin >> n;
+  vector<int> summands = optimal_summands(n);
+  cout << summands.size() << '\n';
+  for (size_t i = 0; i < summands.size(); ++i) {
+    cout << summands[i] << ' ';
   }
 }
